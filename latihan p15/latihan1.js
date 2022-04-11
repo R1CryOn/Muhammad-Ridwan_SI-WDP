0c8be59c49d54d373d1bcf054508d60dbbbaaf8a
R1CryOn <muhhridwan94@gmail.com>
2022-04-11 21:43:02 +0700

    Create latihan1.js

---
+++ latihan p15/latihan1.js
@@ -0,0 +1,10 @@
+function compareNumbers (firstNumber, secondNumber){
+    let firstNumber = 5
+    let secondNumber = 8
+}
+
+console.log(compareNumbers(5,8))
+console.log(compareNumbers(5,3))
+console.log(compareNumbers(4,4))
+console.log(compareNumbers(3,3))
+console.log(compareNumbers(17,2))
