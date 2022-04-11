e7c5fea95c597e52f86cb9333223ffc9f6ce6dec
R1CryOn <muhhridwan94@gmail.com>
2022-04-11 21:43:05 +0700

    Create latihan2.js

---
+++ latihan p15/latihan2.js
@@ -0,0 +1,10 @@
+function reverseString(text) {
+    return text.split("").reverse().join("");
+  }
+
+  console.log(reverseString('hello world'))
+  console.log(reverseString('John Doe'))
+  console.log(reverseString('I am a bookworm'))
+  console.log(reverseString('Coding is My Hooby'))
+  console.log(reverseString('Superrr'))
+
